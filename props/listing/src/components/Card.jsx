8bed2e58e1d0c50c230b2d65d9c1a7fb0437b9c2
Nikita Peplov <npeplov@yandex.ru>
2021-05-27 09:29:58 +0300

    #3.2 props - by second component: card

--- props/listing/src/components/Card.jsx
+++ props/listing/src/components/Card.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
-export default function Card() {
+export default function Card({ item, MainImage }) {
+  function currencySymbol(currency) {
+    if (currency === 'USD') return '$';
+    else if (currency === 'EUR') return '€';
+    return currency + ' ';
+  }
+
+  function titleCut(title) {
+    if (!title) return;
+    if (title.length > 50) return title.slice(0, 50) + '...';
+  }
+
+  function quantityLevel(quantity) {
+    if (quantity <= 10) return 'item-quantity level-low';
+    if (quantity <= 20) return 'item-quantity level-medium';
+    return 'item-quantity level-high';
+  }
+
   return (
-    <div>
-      
+    <div className="item" >
+      <div className="item-image">
+        <a href={item.url}>
+          <img src={MainImage.url_570xN} alt="" />
+        </a>
+      </div>
+      <div className="item-details">
+        <p className="item-title">{titleCut(item.title)}</p>
+        <p className="item-price">
+          {currencySymbol(item.currency_code)}{item.price}
+        </p>
+        <p className={quantityLevel(item.quantity)}>{item.quantity}</p>
+      </div>
     </div>
-  )
+  );
+}
+
+Card.defaultProps = {
+  MainImage: {url_570xN: null},
 }
--- props/listing/src/components/Listing.jsx
+++ props/listing/src/components/Listing.jsx
@@ -1,41 +1,18 @@
 import React from "react";
+import Card from "./Card";
 
 export default function Listing({ items }) {
   return (
     <div className="item-list">
       {items.map((item) => {
-        if (item.state !== "active") {
-          return null;
-        }
-        return (
-          <div className="item" key={item.title}>
-            <div className="item-image">
-              <a href={item.url}>
-                <img src={item.MainImage.url_570xN} alt="" />
-              </a>
-            </div>
-            <div className="item-details" key={item.title}>
-              <p className="item-title">{item.title}</p>
-              <p className="item-price">{item.currency_code} {item.price}</p>
-              <p className="item-quantity level-medium">{item.quantity}</p>
-            </div>
-          </div>
-        );
+        return <Card item={item} MainImage={item.MainImage} key={item.listing_id} />
       })}
     </div>
   );
 }
 
-// в отд. карточку:
-// Listing.defaultProps = {
-//   items.MainImage: null;
-// }
+Listing.defaultProps = {
+  items: [],
+}
 
-// 1. default props вместо null
 
-// Если название предложения привышает 50 символов, то необходимо выводить только 
-// первые 50 символов, и добавлять символ … в конце.
-
-// в долларах США, код USD, то цену вывести в формате $50.00;
-// в евро, код EUR, то цену вывести в формате €50.00;
-// в остальных случаях цену вывести в формате 50.00 GBP, где GBP — код валюты.
