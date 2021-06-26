fbb7a1d2160ae00e0b53f2200c7c7170dd9cee69
Nikita Peplov <npeplov@yandex.ru>
2021-06-26 17:09:18 +0300

    #8.2 hooks-context - useJsonFetch: getData incapsulated

--- hooks-context/use-json-fetch/src/hooks/useJsonFetch.js
+++ hooks-context/use-json-fetch/src/hooks/useJsonFetch.js
@@ -6,26 +6,23 @@ export default function  useJsonFetch(url) {
   const [isLoading, setLoading] = useState(false)
   const [hasError, setError] = useState(false)
 
-  const getData = async () => {
-    setLoading(true)
-    try {
-      const response = await fetch(url)
-
-      if (!response.ok) 
-        throw new Error(response.statusText)
-
-      const data = await response.json()
-      setData(data)
-    }
-    catch(e) {
-      setError(e.toString())
-    }
-    finally {
-      setLoading(false)
-    }
-  }
-
   useEffect(() => {
+    const getData = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch(url)
+        if (!response.ok) 
+          throw new Error(response.statusText)
+        const data = await response.json()
+        setData(data)
+      }
+      catch(e) {
+        setError(e.toString())
+      }
+      finally {
+        setLoading(false)
+      }
+    }
     getData()
   }, [url])
 
